4abd59753036e974a99770806fc5c18dddec283d
Tri Cao <tricq@flextech.vn>
2023-03-02 21:43:28 +0200

    Eliminate the try-catch block

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+require("express-async-errors");
 
 const blogsRouter = require("./controllers/blogs");
 const middleware = require("./utils/middleware");
